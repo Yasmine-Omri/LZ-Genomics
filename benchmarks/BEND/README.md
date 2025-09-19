f4559482d7cd3c2e13ca2f837f9463c21cbd63e0
Yasmine Omri <yasmineomri@college.harvard.edu>
2025-09-18 22:27:20 -0700

    BEND histone

---
+++ benchmarks/BEND/README.md
@@ -0,0 +1,6 @@
+
+===TASK: histone modification
+- Download and unzip this folder in data/
+https://drive.google.com/drive/folders/1LiC3mG3zUHF8XmOuT8K__mjaXrSJ4bp_
+
+- run run_scripts/histone_modification.sh
---
+++ benchmarks/BEND/run_scripts/histone_modification.sh
@@ -0,0 +1,15 @@
+
+
+python ../python_scripts/train_multi_label.py \
+  -dataset_folder "../data/histone_modification" \
+  -pretrain_file "../../../dnabert_2_pretrain/dev.txt" \
+  --include_prev_context "{False}" \
+  --gamma "{0.1, 0.33, 0.5, 0.75, 1, 3, 5}" \
+  --nb_train_iterations "{1, 3, 5, 7, 10}" \
+  --handle_n_setting "{remove}" \
+  --ratio_pretrain_train "{0}" \
+  --ensemble_type "{entropy}" \
+  --num_threads "{64}" \
+  --validation_metric "auroc" \
+  --max_depth "{12}" \
+  > ../outputs/histone_modification.txt
